bc410ff34babf6f9f687ce434e8587aa9d46a0ab
leugimav <leugimav@gmail.com>
2019-08-22 18:52:26 -0500

    Version 1.2 APP Empresa

---
+++ app/src/main/java/com/proyecto/sistemas/mediccityempresa/VisualizarDatos.java
@@ -0,0 +1,14 @@
+package com.proyecto.sistemas.mediccityempresa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisualizarDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_datos);
+    }
+}
